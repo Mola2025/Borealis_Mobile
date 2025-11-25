773bd2f30f8704d710ee083151a35dc566c9af0b
Juan <kylelovescookies047@gmail.com>
2025-11-24 22:20:26 -0500

    Basic EditProfileActivity and activity_edit_profile.xml

--- app/src/main/java/com/example/borealis_mobile/EditProfileActivity.java
+++ app/src/main/java/com/example/borealis_mobile/EditProfileActivity.java
@@ -1,6 +1,9 @@
 package com.example.borealis_mobile;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +13,30 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class EditProfileActivity extends AppCompatActivity {
 
+    ImageView editProfileImage;
+    EditText editUsername, editEmail;
+    Button btnSaveProfile, btnBuyPremium;
+    private final int PREMIUM_PRICE = 200;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_edit_profile);
+
+        editProfileImage = findViewById(R.id.edit_profile_image);
+        editUsername = findViewById(R.id.edit_username_input);
+        editEmail = findViewById(R.id.edit_email_input);
+        btnSaveProfile = findViewById(R.id.btn_save_profile);
+        btnBuyPremium = findViewById(R.id.btn_buy_premium);
+
+        btnSaveProfile.setOnClickListener(v -> {
+
+        });
+
+        btnBuyPremium.setOnClickListener(v -> {
+
+        });
+
     }
 }

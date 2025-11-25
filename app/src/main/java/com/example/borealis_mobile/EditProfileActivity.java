0bbb091828bdb1a3fb39e7ae0b771876758ea1bc
David Molano <davidm0l4n0@gmail.com>
2025-11-24 23:41:50 -0500

    David Molano
    - Update Premium Status Working Successfully
    - Saving changes into DB correctly
    - Allows now to buy premium items :D

--- app/src/main/java/com/example/borealis_mobile/EditProfileActivity.java
+++ app/src/main/java/com/example/borealis_mobile/EditProfileActivity.java
@@ -2,7 +2,6 @@ package com.example.borealis_mobile;
 
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -11,25 +10,48 @@ import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.stripe.android.PaymentConfiguration;
+import com.stripe.android.paymentsheet.PaymentSheet;
+import com.stripe.android.paymentsheet.PaymentSheetResult;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class EditProfileActivity extends AppCompatActivity {
 
     ImageButton editProfileImage;
     EditText editUsername, editEmail;
     Button btnSaveProfile, btnBuyPremium;
-
-
     private FirebaseAuth firebaseAuth;
     private StorageReference storageReference;
+    private DatabaseReference databaseUsers;
     private Uri imageUri = null;
     private final int PREMIUM_PRICE = 200;
+    // Esta es para el stripe porque se necesitan en cents
+    private final long premiumAmountCents = PREMIUM_PRICE * 100;
+
+    APIKeys apiKeys = new APIKeys();
+    PaymentSheet paymentSheet;
     private final ActivityResultLauncher<String> selectImageLauncher =
             registerForActivityResult(new ActivityResultContracts.GetContent(), uri -> {
                 if (uri != null) {
@@ -52,6 +74,12 @@ public class EditProfileActivity extends AppCompatActivity {
         btnSaveProfile = findViewById(R.id.btn_save_profile);
         btnBuyPremium = findViewById(R.id.btn_buy_premium);
 
+        storageReference = FirebaseStorage.getInstance().getReference("profile_images");
+        databaseUsers = FirebaseDatabase.getInstance().getReference("users");
+
+        PaymentConfiguration.init(this, apiKeys.getPublishableKey());
+        paymentSheet = new PaymentSheet(this, this::onPaymentResultPremium);
+
         editProfileImage.setOnClickListener(v -> {
             selectImageLauncher.launch("image/*");
         });
@@ -61,17 +89,37 @@ public class EditProfileActivity extends AppCompatActivity {
         });
 
         btnBuyPremium.setOnClickListener(v -> {
-            // TODO: aca pones el coso de pagar con el PREMIUM_PRICE
+            getClientSecretPremium();
         });
+
+        loadUserData();
     }
+
     private void loadUserData() {
         FirebaseUser user = firebaseAuth.getCurrentUser();
         if (user != null) {
-            editUsername.setText(user.getDisplayName());
             editEmail.setText(user.getEmail());
+            editEmail.setEnabled(false);
+
+            databaseUsers.child(user.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (snapshot.exists()) {
+                        String currentUsername = snapshot.child("username").getValue(String.class);
+                        editUsername.setText(currentUsername);
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Toast.makeText(EditProfileActivity.this, "Error loading user data", Toast.LENGTH_SHORT).show();
+                }
+            });
+
             editProfileImage.setImageURI(user.getPhotoUrl());
         }
     }
+
     private void saveProfile() {
         String newUsername = editUsername.getText().toString();
         FirebaseUser user = firebaseAuth.getCurrentUser();
@@ -96,6 +144,7 @@ public class EditProfileActivity extends AppCompatActivity {
             updateUserProfile(user, newUsername, String.valueOf(user.getPhotoUrl()));
         }
     }
+
     private void updateUserProfile(FirebaseUser user, String username, String imageURL) {
         UserProfileChangeRequest.Builder builder = new UserProfileChangeRequest.Builder()
                 .setDisplayName(username);
@@ -107,10 +156,95 @@ public class EditProfileActivity extends AppCompatActivity {
         user.updateProfile(builder.build())
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
-                        Toast.makeText(this, "Profile Updated!", Toast.LENGTH_SHORT).show();
+
+                        DatabaseReference userRef = FirebaseDatabase.getInstance()
+                                .getReference("users")
+                                .child(user.getUid());
+
+                        Map<String, Object> updates = new HashMap<>();
+                        updates.put("username", username);
+                        updates.put("profileImageURL", imageURL);
+
+                        userRef.updateChildren(updates)
+                                .addOnSuccessListener(a -> {
+                                    Toast.makeText(this,
+                                            "Profile Updated!",
+                                            Toast.LENGTH_SHORT).show();
+                                })
+                                .addOnFailureListener(e -> {
+                                    Toast.makeText(this,
+                                            "Error updating DB",
+                                            Toast.LENGTH_SHORT).show();
+                                });
+
                     } else {
                         Toast.makeText(this, "Error updating profile", Toast.LENGTH_SHORT).show();
                     }
+                });
+    }
+
+    private void getClientSecretPremium() {
+
+        String url = "https://api.stripe.com/v1/payment_intents";
+
+        StringRequest request = new StringRequest(
+                Request.Method.POST,
+                url,
+                response -> {
+                    try {
+                        JSONObject obj = new JSONObject(response);
+                        String client_secret = obj.getString("client_secret");
+
+                        paymentSheet.presentWithPaymentIntent(
+                                client_secret,
+                                new PaymentSheet.Configuration("Stripe"));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                },
+                error -> Toast.makeText(this, error.getLocalizedMessage(), Toast.LENGTH_SHORT).show()
+        ) {
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> h = new HashMap<>();
+                h.put("Authorization", "Bearer " + apiKeys.getSecretKey());
+                return h;
+            }
+
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> p = new HashMap<>();
+                p.put("amount", String.valueOf(premiumAmountCents)); // 200 USD
+                p.put("currency", "usd");
+                p.put("automatic_payment_methods[enabled]", "true");
+                return p;
+            }
+        };
+
+        Volley.newRequestQueue(this).add(request);
+    }
+
+    private void onPaymentResultPremium(PaymentSheetResult result) {
+        if (result instanceof PaymentSheetResult.Completed) {
+            upgradeUserToPremium();
+        }
+    }
+
+    private void upgradeUserToPremium() {
+
+        String uid = FirebaseAuth.getInstance().getUid();
+
+        FirebaseDatabase.getInstance()
+                .getReference("users")
+                .child(uid)
+                .child("isPremium")
+                .setValue(true)
+                .addOnSuccessListener(a -> {
+                    Toast.makeText(this, "Premium activated! Thank You For Upgrading!", Toast.LENGTH_LONG).show();
+                })
+                .addOnFailureListener(e -> {
+                    Toast.makeText(this, "Error upgrading premium", Toast.LENGTH_SHORT).show();
                 });
     }
 }
